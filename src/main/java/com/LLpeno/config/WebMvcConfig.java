63e0f0938880e8c76a15dee8851a5e54b3623682
LLpeno <li943945876@gmail.com>
2022-09-06 14:21:22 +0800

    third submit

--- src/main/java/com/LLpeno/config/WebMvcConfig.java
+++ src/main/java/com/LLpeno/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.LLpeno.config;
 
+import com.LLpeno.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * @author LLPeno
@@ -23,5 +28,18 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("静态资源开始映射....");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器");
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，讲java对象转换成JSON对象
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+
+        // 将上面的消息转换器对象追加到mvc框架的转换器容器集合中
+        converters.add(0,messageConverter);
+
+
     }
 }
--- src/main/java/com/LLpeno/controller/EmployeeController.java
+++ src/main/java/com/LLpeno/controller/EmployeeController.java
@@ -4,15 +4,15 @@ import com.LLpeno.common.R;
 import com.LLpeno.entity.Employee;
 import com.LLpeno.service.IEmployeeService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 
 /**
@@ -57,6 +57,7 @@ public class EmployeeController {
 
         //6. 登录成功，将员工id存入Session并返回登录成功结果
         request.getSession().setAttribute("employee",emp.getId());
+        log.info("session传入成功");
         return R.success(emp);
     }
 
@@ -66,4 +67,77 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
     }
+
+    // 新增员工
+    @PostMapping
+    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工，员工信息：{}",employee.toString());
+    //设置初始密码123456，需要进行MD5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empID = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empID);
+        employee.setUpdateUser(empID);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+    }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        log.info("page={},pageSize={},name={}",page,pageSize,name);
+
+        // 构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+        //构造条件构造器(动态封装后续传入的参数，如name）
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        // 添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        // 添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        // 执行查询
+        employeeService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id修改员工信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+
+       Long empID = (Long) request.getSession().getAttribute("employee");
+       employee.setUpdateUser(empID);
+       employee.setUpdateTime(LocalDateTime.now());
+       employeeService.updateById(employee);
+       return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getByID(@PathVariable Long id){
+        log.info("根据id查询员工信息");
+        Employee employee = employeeService.getById(id);
+        if(employee != null) {
+            return R.success(employee);
+        }
+        return R.error("没有查询到相应员工信息");
+    }
+
 }
